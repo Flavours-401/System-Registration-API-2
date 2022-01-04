3f7b8ab872505c6506ddc532517f86e079af86e2
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2022-01-04 11:37:31 +0200

    add session serilaizer, url & views

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -10,19 +10,21 @@ from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs
+from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs, SessionYearModel
 from .permissions import IsOwnerOrReadOnly
-from .serializer import student_management_Serializer,Admin_HOD_Serializer,Staff_Serializer, Course_Serializer, Subject_Serializer, Student_Serializer, Attendance_Serializer, Attendance_Report_Serializer, Leave_Student_Report_Serializer, Leave_Staff_Report_Serializer, Feedback_Student_Serializer, Feedback_Staff_Serializer, Notification_Student_Serializer, Notification_Staffs_Serializer
+from .serializer import student_management_Serializer, Admin_HOD_Serializer, Staff_Serializer, Course_Serializer, Subject_Serializer, Student_Serializer, Attendance_Serializer, Attendance_Report_Serializer, Leave_Student_Report_Serializer, Leave_Staff_Report_Serializer, Feedback_Student_Serializer, Feedback_Staff_Serializer, Notification_Student_Serializer, Notification_Staffs_Serializer, SessionYearModel_Serializer
 
 
 class CustomUserList(ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = student_management_Serializer
 
+
 class CustomUserListDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     queryset = CustomUser.objects.all()
     serializer_class = student_management_Serializer
+
 
 class AdminHODList(ListCreateAPIView):
     queryset = AdminHOD.objects.all()
@@ -34,28 +36,34 @@ class AdminHODListDetail(RetrieveUpdateDestroyAPIView):
     queryset = AdminHOD.objects.all()
     serializer_class = Admin_HOD_Serializer
 
+
 class StaffsList(ListCreateAPIView):
     queryset = Staffs.objects.all()
     serializer_class = Staff_Serializer
+
 
 class StaffsListDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = Staffs.objects.all()
     serializer_class = Staff_Serializer
 
+
 class CoursesDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = Courses.objects.all()
     serializer_class = Course_Serializer
 
+
 class CoursesList(ListCreateAPIView):
     queryset = Courses.objects.all()
     serializer_class = Course_Serializer
+
 
 class SubjectsDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = Subjects.objects.all()
     serializer_class = Subject_Serializer
+
 
 class SubjectsList(ListCreateAPIView):
     queryset = Subjects.objects.all()
@@ -67,81 +75,110 @@ class StudentsDetail(RetrieveUpdateDestroyAPIView):
     queryset = Students.objects.all()
     serializer_class = Student_Serializer
 
+
 class StudentsList(ListCreateAPIView):
     queryset = Students.objects.all()
     serializer_class = Student_Serializer
+
+
+class SessionYearDetail(RetrieveUpdateDestroyAPIView):
+    # permission_classes = (HasAdminOrReadOnly,)
+    queryset = SessionYearModel.objects.all()
+    serializer_class = SessionYearModel_Serializer
+
+
+class SessionYearList(ListCreateAPIView):
+    queryset = SessionYearModel.objects.all()
+    serializer_class = SessionYearModel_Serializer
+
 
 class AttendanceDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = Attendance.objects.all()
     serializer_class = Attendance_Serializer
 
+
 class AttendanceList(ListCreateAPIView):
     queryset = Attendance.objects.all()
     serializer_class = Attendance_Serializer
+
 
 class AttendanceReportDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = AttendanceReport.objects.all()
     serializer_class = Attendance_Report_Serializer
 
+
 class AttendanceReportList(ListCreateAPIView):
     queryset = AttendanceReport.objects.all()
     serializer_class = Attendance_Report_Serializer
+
 
 class LeaveReportStudentDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = LeaveReportStudent.objects.all()
     serializer_class = Leave_Student_Report_Serializer
 
+
 class LeaveReportStudentList(ListCreateAPIView):
-    queryset =LeaveReportStudent.objects.all()
+    queryset = LeaveReportStudent.objects.all()
     serializer_class = Leave_Student_Report_Serializer
+
 
 class LeaveReportStaffDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = LeaveReportStaff.objects.all()
     serializer_class = Leave_Staff_Report_Serializer
 
+
 class LeaveReportStaffList(ListCreateAPIView):
-    queryset =LeaveReportStaff.objects.all()
+    queryset = LeaveReportStaff.objects.all()
     serializer_class = Leave_Staff_Report_Serializer
+
 
 class FeedBackStudentDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = FeedBackStudent.objects.all()
     serializer_class = Feedback_Student_Serializer
 
+
 class FeedBackStudentList(ListCreateAPIView):
-    queryset =FeedBackStudent.objects.all()
+    queryset = FeedBackStudent.objects.all()
     serializer_class = Feedback_Student_Serializer
+
 
 class FeedBackStaffsDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = FeedBackStaffs.objects.all()
     serializer_class = Feedback_Staff_Serializer
 
+
 class FeedBackStaffsList(ListCreateAPIView):
-    queryset =FeedBackStaffs.objects.all()
+    queryset = FeedBackStaffs.objects.all()
     serializer_class = Feedback_Staff_Serializer
+
 
 class NotificationStudentDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = NotificationStudent.objects.all()
     serializer_class = Notification_Student_Serializer
 
+
 class NotificationStudentList(ListCreateAPIView):
-    queryset =NotificationStudent.objects.all()
+    queryset = NotificationStudent.objects.all()
     serializer_class = Notification_Student_Serializer
+
 
 class NotificationStaffsDetail(RetrieveUpdateDestroyAPIView):
     # permission_classes = (IsOwnerOrReadOnly,)
     queryset = NotificationStaffs.objects.all()
     serializer_class = Notification_Staffs_Serializer
 
+
 class NotificationStaffsList(ListCreateAPIView):
-    queryset =NotificationStaffs.objects.all()
+    queryset = NotificationStaffs.objects.all()
     serializer_class = Notification_Staffs_Serializer
+
 
 def home(request):
     return render(request, 'index.html')
@@ -151,23 +188,23 @@ def loginPage(request):
     return render(request, 'login.html')
 
 
-
 def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        user = EmailBackEnd.authenticate(request, username=request.POST.get(
+            'email'), password=request.POST.get('password'))
         if user != None:
             login(request, user)
             user_type = user.user_type
-            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
+            # return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
             if user_type == '1':
                 return redirect('admin_home')
-                
+
             elif user_type == '2':
                 # return HttpResponse("Staff Login")
                 return redirect('staff_home')
-                
+
             elif user_type == '3':
                 # return HttpResponse("Student Login")
                 return redirect('student_home')
@@ -176,9 +213,8 @@ def doLogin(request):
                 return redirect('login')
         else:
             messages.error(request, "Invalid Login Credentials!")
-            #return HttpResponseRedirect("/")
+            # return HttpResponseRedirect("/")
             return redirect('login')
-
 
 
 def get_user_details(request):
@@ -188,9 +224,6 @@ def get_user_details(request):
         return HttpResponse("Please Login First")
 
 
-
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
-
-
